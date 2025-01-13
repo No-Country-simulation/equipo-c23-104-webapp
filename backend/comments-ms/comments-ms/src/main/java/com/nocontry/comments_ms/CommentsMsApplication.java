bc2b5639560f015aec9656011d3cb89cd10293a5
Walter Alvarado <walvdev@gmail.com>
2025-01-13 13:50:48 -0500

    create comments-ms base,Update docker-compose

---
+++ backend/comments-ms/comments-ms/src/main/java/com/nocontry/comments_ms/CommentsMsApplication.java
@@ -0,0 +1,13 @@
+package com.nocontry.comments_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentsMsApplication.class, args);
+	}
+
+}
---
+++ backend/comments-ms/comments-ms/src/test/java/com/nocontry/comments_ms/CommentsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocontry.comments_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
