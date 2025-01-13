1726bbad1011a8a4698f7a36b9583c15406c87a3
Lucas Ybanez <luck.facebook@hotmail.com>
2025-01-13 15:40:37 -0300

    creé la pagina welcome

---
+++ frontend/src/pages/welcome/Welcome.jsx
@@ -0,0 +1,20 @@
+const Welcome = () => {
+    return (
+        <>
+            <section className="flex">
+                <div>
+                    <h1 className="font-sans text-2xl">Parolu!</h1>
+                    <h2></h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi natus est magni sit, qui vero ex quo ullam blanditiis nobis voluptate. Aspernatur molestiae consectetur ex ratione exercitationem, animi vel autem?</p>
+                    <a href="">Registrarse</a>
+                    <a href="">Iniciar sesión</a>
+                </div>
+                <div>
+
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default Welcome;
