3a9c92c7962297e1bccfb8b249bfed08dc9d7118
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-01-15 03:19:33 -0300

    buscador-nav: creación componente Publications

---
+++ frontend/src/components/Publications.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Publications() {
+  return (
+    <article class="p-4 sm:ml-64 max-w-3xl mx-auto">
+      <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+        <div class="flex items-center justify-center h-40 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+          <p class="text-2xl text-gray-400 dark:text-gray-500">publicacion 1</p>
+        </div>
+        <div class="flex items-center justify-center h-40 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+          <p class="text-2xl text-gray-400 dark:text-gray-500">publicacion 2</p>
+        </div>
+        <div class="flex items-center justify-center h-40 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+          <p class="text-2xl text-gray-400 dark:text-gray-500">publicacion 3</p>
+        </div>
+        <div class="flex items-center justify-center h-40 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+          <p class="text-2xl text-gray-400 dark:text-gray-500">publicacion 4</p>
+        </div>
+        <div class="flex items-center justify-center h-40 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+          <p class="text-2xl text-gray-400 dark:text-gray-500">publicacion 5</p>
+        </div>
+      </div>
+    </article>
+  );
+}
