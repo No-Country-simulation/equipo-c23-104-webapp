1793b25b0d4c26afd1cb04fb0e357cbaf14c3530
Lucasybanez <loquendero31@gmail.com>
2025-01-16 10:36:12 -0300

    textos adaptados a responsive

--- frontend/src/pages/welcome/Welcome.jsx
+++ frontend/src/pages/welcome/Welcome.jsx
@@ -68,7 +68,7 @@ const Welcome = () => {
 <section className="bg-white pt-20">
     <div>
         <div className="container mx-auto text-center">
-            <h2 className="text-6xl text-lime-600 font-bold mb-10">¿Por qué Parolu?</h2>
+            <h2 className="text-4xl lg:text-7xl text-lime-600 font-bold mb-10">¿Por qué Parolu?</h2>
             <p className="text-xl text-gray-700 mb-10">Parolu es la plataforma perfecta para mejorar tus habilidades lingüísticas. Con nuestra comunidad global, podrás:</p>
             </div>
     </div>
@@ -97,37 +97,43 @@ const Welcome = () => {
             </div>
         </div>
         <div className="text-center md:text-left">
-            <h3 className="text-6xl text-lime-600 font-bold mb-10">Crea tu cuenta</h3>
-            <p className="text-3xl text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi molestiae aspernatur exercitationem sequi hic nihil velit dolorem eveniet.</p>
+            <h3 className="text-4xl lg:text-6xl text-lime-600 font-bold mb-10">Crea tu cuenta</h3>
+            <p className="text-xl lg:text-3xl text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi molestiae aspernatur exercitationem sequi hic nihil velit dolorem eveniet.</p>
         </div>
 
         {/* Segunda Fila: Texto | Imagen */}
         <div className="text-center md:text-right">
-            <h3 className="text-6xl text-lime-600 font-bold mb-10">Mejora tus Habilidades</h3>
-            <p className="text-3xl text-gray-700">Perfecciona tu gramática, vocabulario y pronunciación.</p>
+            <h3 className="text-4xl lg:text-6xl text-lime-600 font-bold mb-10">Mejora tus Habilidades</h3>
+            <p className="text-xl lg:text-3xl text-gray-700">Perfecciona tu gramática, vocabulario y pronunciación.</p>
         </div>
-        <div>
-            <img src={fotoP} alt="Beneficio 2" className="rounded-md"/>
+        <div className='w-auto'>
+            <div className="mx-20 relative">
+                <div className="absolute w-full inset-0 -translate-x-4 translate-y-4 bg-gradient-to-r from-emerald-400 to-lime-600 rounded-md shadow-lg"></div>
+                <img src={picture1} alt="Beneficio 1" className="relative w-full rounded-md z-10"/>
+            </div>
         </div>
 
         {/* Tercera Fila: Imagen | Texto */}
-        <div>
-            <img src={capture} alt="Beneficio 3" className="rounded-md"/>
+        <div className='w-auto'>
+            <div className="mx-20 relative">
+                <div className="absolute w-full inset-0 -translate-x-4 translate-y-4 bg-gradient-to-r from-emerald-400 to-lime-600 rounded-md shadow-lg"></div>
+                <img src={picture1} alt="Beneficio 1" className="relative w-full rounded-md z-10"/>
+            </div>
         </div>
         <div className="text-center md:text-left">
-            <h3 className="text-6xl text-lime-600 font-bold mb-10">Haz Nuevos Amigos</h3>
-            <p className="text-3xl text-gray-700">Conéctate con personas de todo el mundo y crea nuevas amistades.</p>
+            <h3 className="text-4xl lg:text-6xl text-lime-600 font-bold mb-10">Haz Nuevos Amigos</h3>
+            <p className="text-xl lg:text-3xl text-gray-700">Conéctate con personas de todo el mundo y crea nuevas amistades.</p>
         </div>
     </div>
 </section>
 
 
 {/* Sección Casos de Éxito */}
-<section className="bg-white py-20">
+<section className="bg-white p-10">
     <div className="container mx-auto text-center">
-        <h2 className="text-4xl font-bold mb-10">Casos de Éxito</h2>
+        <h2 className="text-4xl text-lime-600 font-bold mb-10">Casos de Éxito</h2>
         <p className="text-xl text-gray-700 mb-5">Miles de estudiantes han alcanzado sus metas con Parolu. ¡Conoce sus historias de éxito!</p>
-        <div className="grid grid-cols-2 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
             <div className="bg-gray-100 p-8 rounded-md shadow-md">
                 <p className="text-gray-700">"Gracias a Parolu, ahora puedo conversar con confianza en mi nuevo idioma. ¡Es una experiencia maravillosa!"</p>
                 <p className="text-lime-600 font-bold mt-4">- María García</p>
@@ -143,14 +149,14 @@ const Welcome = () => {
 {/* Sección de Usuarios Totales y Puntuación Media */}
 <section className="bg-white py-20">
     <div className="container mx-auto text-center">
-        <h2 className="text-4xl font-bold mb-10">Nuestra Comunidad</h2>
+        <h2 className="text-4xl text-gray-700 font-bold mb-10">Nuestra Comunidad</h2>
         <div className="grid grid-cols-2 gap-10">
             <div>
-                <h3 className="text-6xl font-bold text-lime-600">1M+</h3>
+                <h3 className="text-5xl md:text-6xl font-bold text-lime-600">1M+</h3>
                 <p className="text-xl text-gray-700">Usuarios Totales</p>
             </div>
             <div>
-                <h3 className="text-6xl font-bold text-lime-600">4.8/5</h3>
+                <h3 className="text-5xl md:text-6xl font-bold text-lime-600">4.8/5</h3>
                 <p className="text-xl text-gray-700">Puntuación Media</p>
             </div>
         </div>
@@ -160,8 +166,8 @@ const Welcome = () => {
 {/* Sección de Llamada a la Acción */}
 <section className="bg-gradient-to-r from-emerald-700 to-lime-700 py-20">
     <div className="container mx-auto text-center">
-        <h2 className="text-4xl font-bold text-white mb-5">¡Únete a Parolu Hoy!</h2>
-        <p className="text-xl text-white mb-10">Empieza a mejorar tus habilidades lingüísticas con nuestra comunidad global. ¡No esperes más!</p>
+        <h2 className="text-3xl md:text-4xl font-bold text-white mb-5">¡Únete a Parolu Hoy!</h2>
+        <p className="md:text-xl text-white mb-10">Empieza a mejorar tus habilidades lingüísticas con nuestra comunidad global. ¡No esperes más!</p>
         <a href="" className='h-10 px-6 py-4 font-semibold rounded-md bg-white text-lime-600 mr-5'>Registrarse</a>
         <a href="" className='h-10 px-6 py-4 font-semibold rounded-md bg-transparent text-white border border-white'>Iniciar sesión</a>
     </div>
