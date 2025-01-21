ac0fdc134df4ca56792f21111238dd59368460c1
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-01-21 17:02:03 -0300

    buscador-nav: Modificación dropdown extra, agregado boton darkmode

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -82,27 +82,11 @@ export default function Navbar({ toggleTextVisibility, setSearchQuery }) {
             </div>
           </form>
           <div className="flex items-center">
-            <button
-              id="dark-mode-toggle"
-              onClick={toggleDarkMode}
-              className="text-sm p-2 rounded-full focus:ring-0 focus:outline-none bg-gray-200 dark:bg-[#4A494A] dark:text-white"
-            >
-              {darkMode ? (
-                <i
-                  className="fa-solid fa-sun text-yellow-400 rounded-full w-6 h-5"
-                  aria-hidden="true"
-                ></i>
-              ) : (
-                <i
-                  className="fa-solid fa-moon text-gray-600 dark:text-gray-200 rounded-full w-6 h-5"
-                  aria-hidden="true"
-                ></i>
-              )}
-            </button>
+            {/* Dropdown de Usuario */}
             <div className="relative flex items-center ms-3">
               <a
                 id="user-button"
-                className="flex text-sm border-3 rounded-full w-11 h-11 overflow-hidden hover:shadow-lg focus:ring-0 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:hover:border-green-600 dark:hover:shadow-lg dark:focus:ring-0 dark:focus:border-green-500 transition-all duration-300"
+                className="flex border-3 rounded-full w-11 h-11 overflow-hidden hover:shadow-lg focus:ring-0 focus:outline-none dark:hover:shadow-lg dark:focus:ring-0 transition-all duration-300"
                 aria-expanded={isDropdownOpen ? "true" : "false"}
                 onClick={toggleDropdown}
               >
@@ -135,7 +119,7 @@ export default function Navbar({ toggleTextVisibility, setSearchQuery }) {
                       href="#"
                       className="text-[12px] mx-2 mt-2 rounded border border-transparent block px-4 py-2 text-gray-900 transition-all duration-100 hover:bg-[#00bf00] hover:text-white hover:shadow-inner group dark:text-white dark:hover:border-white dark:hover:text-white"
                     >
-                      <i className="fa-solid fa-user mr-2 text-[#00bf00] dark:text-white transition-all duration-300 group-hover:text-white"></i>{" "}
+                      <i className="fa-solid fa-user mr-2 text-[#00bf00] dark:text-white transition-all duration-300 group-hover:text-white"></i>
                       Panel de perfil
                     </a>
                   </li>
@@ -144,7 +128,7 @@ export default function Navbar({ toggleTextVisibility, setSearchQuery }) {
                       href="#"
                       className="text-[12px] mx-2 rounded border border-transparent block px-4 py-2 text-gray-900 transition-all duration-100 hover:bg-[#00bf00] hover:text-white hover:shadow-inner group dark:text-white dark:hover:border-white dark:hover:text-white"
                     >
-                      <i className="fa-solid fa-gear mr-2 text-[#00bf00] dark:text-white transition-all duration-300 group-hover:text-white"></i>{" "}
+                      <i className="fa-solid fa-gear mr-2 text-[#00bf00] dark:text-white transition-all duration-300 group-hover:text-white"></i>
                       Configuración
                     </a>
                   </li>
@@ -187,50 +171,69 @@ export default function Navbar({ toggleTextVisibility, setSearchQuery }) {
                 </div>
               </div>
             </div>
-          </div>
-          <div className="relative">
-            <button
-              onClick={toggleSubmenu}
-              className="inline-flex items-center  p-2 text-sm font-medium text-gray-900 bg-white rounded-lg hover:bg-gray-100 dark:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-            >
-              <svg
-                className="w-5 h-5 ml-1"
-                fill="currentColor"
-                viewBox="0 0 4 15"
+
+            {/* Dropdown de Opciones */}
+            <div className="relative flex items-center ms-3">
+              <button
+                onClick={toggleSubmenu}
+                className="flex items-center justify-center rounded-full p-2 text-sm font-medium border-1 border-transparent hover:border-transparent text-gray-900 bg-white hover:bg-[#00bf00] group dark:hover:border-white dark:text-white dark:bg-[#4A494A] dark:hover:bg-[#00bf00] transition-all duration-300"
               >
-                <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
-              </svg>
-            </button>
-            {isSubmenuOpen && (
-              <div className="absolute right-0 mt-4 w-44 bg-white divide-y divide-gray-100 rounded shadow-lg dark:bg-gray-700 dark:divide-gray-600">
-                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+                <svg
+                  className="w-5 h-5 text-[#00bf00] dark:text-white group-hover:text-white"
+                  fill="currentColor"
+                  viewBox="0 0 4 15"
+                >
+                  <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
+                </svg>
+              </button>
+              <div
+                className={`absolute top-full right-0 mt-4 py-2 z-50 w-44 bg-white divide-y divide-gray-100 rounded shadow-lg dark:bg-[#4A494A] dark:divide-gray-600 border border-[#A19FA1] transition-all duration-300 ease-out transform ${
+                  isSubmenuOpen
+                    ? "scale-100 opacity-100"
+                    : "scale-95 opacity-0 pointer-events-none"
+                }`}
+              >
+                <ul className="space-y-0">
                   <li>
                     <a
                       href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      className="text-[12px] mx-2 mt-0 rounded border border-transparent block px-4 py-2 text-gray-900 transition-all duration-100 hover:bg-[#00bf00] hover:text-white hover:shadow-inner group dark:text-white dark:hover:border-white dark:hover:text-white"
                     >
+                      <i className="fa-solid fa-globe mr-2 text-[#00bf00] dark:text-white transition-all duration-300 group-hover:text-white"></i>
                       Idioma
                     </a>
                   </li>
                   <li>
                     <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      id="dark-mode-toggle"
+                      onClick={toggleDarkMode}
+                      className="text-[12px] mx-2 rounded border border-transparent block px-4 py-2 text-gray-900 transition-all duration-100 hover:bg-[#00bf00] hover:text-white hover:shadow-inner group dark:text-white dark:hover:border-white dark:hover:text-white"
                     >
-                      Modo
+                      {darkMode ? (
+                        <>
+                          <i className="fa-solid fa-sun mr-2 text-[#00bf00] dark:text-white transition-all duration-300 group-hover:text-white"></i>
+                          <span>Modo Claro</span>
+                        </>
+                      ) : (
+                        <>
+                          <i className="fa-solid fa-moon mr-2 text-[#00bf00] dark:text-white transition-all duration-300 group-hover:text-white"></i>
+                          <span>Modo Oscuro</span>
+                        </>
+                      )}
                     </a>
                   </li>
                   <li>
                     <a
                       href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      className="text-[12px] mx-2 rounded border border-transparent block px-4 py-2 text-gray-900 transition-all duration-100 hover:bg-[#00bf00] hover:text-white hover:shadow-inner group dark:text-white dark:hover:border-white dark:hover:text-white"
                     >
+                      <i className="fa-solid fa-circle-question mr-2 text-[#00bf00] dark:text-white transition-all duration-300 group-hover:text-white"></i>{" "}
                       Ayuda
                     </a>
                   </li>
                 </ul>
               </div>
-            )}
+            </div>
           </div>
         </div>
       </div>
