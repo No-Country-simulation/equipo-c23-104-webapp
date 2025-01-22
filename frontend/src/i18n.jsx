523a8a43dd1283c4651c79b10f748f1347c6cc6e
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-01-22 20:01:09 -0300

    buscador-nav: Agregado traducción esperanto

--- frontend/src/i18n.jsx
+++ frontend/src/i18n.jsx
@@ -4,12 +4,14 @@ import { initReactI18next } from "react-i18next";
 import translationES from "./language/es/translation.json";
 import translationEN from "./language/en/translation.json";
 import translationPT from "./language/pt/translation.json";
+import translationESP from "./language/esp/translation.json";
 
 export default i18next.use(initReactI18next).init({
   resources: {
     es: { translation: translationES },
     en: { translation: translationEN },
     pt: { translation: translationPT },
+    esp: { translation: translationESP },
   },
   lng: "es",
   fallbackLng: "es",
