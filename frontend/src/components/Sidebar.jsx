0cc45d2869fc8e7151ee2fef48802b1f45449730
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-01-30 16:21:44 -0300

    buscador-nav: corrección diseño responsive navbar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,148 +1,102 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 
 export default function Sidebar({ isTextVisible, showText }) {
   const { t } = useTranslation();
 
   return (
-    <aside
-      id="logo-sidebar"
-      className={`fixed top-0 left-0 z-40 h-screen rounded pt-20 bg-white sm:translate-x-0 dark:bg-[#4A494A] dark:border-gray-700 transition-all duration-500 border border-[#A19FA1] ${
-        isTextVisible ? "w-64" : "w-16"
-      }`}
-      aria-label="Sidebar"
-    >
-      <div className="h-full px-3 pb-4 pt-3 overflow-y-auto bg-white dark:bg-[#4A494A] mt-4 transition-all duration-500">
-        <ul className="space-y-0 font-medium ">
-          <li
-            className={`flex items-center px-3 py-2 text-gray-900 rounded hover:bg-[#00bf00] transition-all duration-100 hover:text-white hover:shadow-inner group border border-transparent dark:text-white dark:hover:border-white dark:hover:text-white group text-[14px] ${
-              isTextVisible ? "w-56 h-10" : "w-10 h-10 rounded-full"
+    <>
+      <aside
+        id="logo-sidebar"
+        className={`fixed top-0 left-0 z-40 h-screen rounded pt-20 bg-white sm:translate-x-0 dark:bg-[#4A494A] dark:border-gray-700 transition-all duration-500 border border-[#A19FA1] ${
+          isTextVisible ? "w-64" : "w-16"
+        } hidden sm:block`}
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 pb-4 pt-3 overflow-y-auto bg-white dark:bg-[#4A494A] mt-4 transition-all duration-500">
+          <ul className="space-y-0 font-medium">
+            {[
+              { icon: "fa-house", label: t("home") },
+              { icon: "fa-users", label: t("community") },
+              { icon: "fa-bell", label: t("notifications") },
+              { icon: "fa-right-from-bracket", label: t("logout") },
+            ].map((item, index) => (
+              <li
+                key={index}
+                className={`flex items-center px-3 py-2 text-gray-900 rounded hover:bg-[#00bf00] transition-all duration-100 hover:text-white hover:shadow-inner group border border-transparent dark:text-white dark:hover:border-white dark:hover:text-white group text-[14px] ${
+                  isTextVisible ? "w-56 h-10" : "w-10 h-10 rounded-full"
+                }`}
+              >
+                <a
+                  className={`flex items-center w-full h-full transition-opacity duration-500 ease-in-out ${
+                    showText ? "opacity-100" : "opacity-0"
+                  }`}
+                  href="#"
+                >
+                  <i
+                    className={`fa-solid ${item.icon} text-[#00bf00] dark:text-white transition-all duration-100 group-hover:text-white`}
+                  ></i>
+                  {isTextVisible && (
+                    <span
+                      className={`ml-3 text-[#4A494A] group-hover:text-white dark:text-white transition-all duration-200 ease-in-out transform ${
+                        isTextVisible
+                          ? "opacity-100 translate-x-0"
+                          : "opacity-0 -translate-x-2 w-0 overflow-hidden"
+                      }`}
+                    >
+                      {item.label}
+                    </span>
+                  )}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <button
+            title={!isTextVisible ? t("create") : ""}
+            type="button"
+            className={`border hover:border-transparent bg-[#00bf00] text-white dark:hover:border-white font-medium rounded transition-all duration-200 mt-3 ${
+              isTextVisible
+                ? "w-56 h-auto px-3 py-2 text-[14px]"
+                : "w-10 h-10 flex items-center justify-center text-[10px] rounded-full"
             }`}
           >
-            <a
-              className={`flex items-center w-full h-full transition-opacity duration-500 ease-in-out ${
-                showText ? "opacity-100" : "opacity-0"
-              }`}
-              href="#"
-              title={!isTextVisible ? "Inicio" : ""}
-            >
-              <i className="fa-solid fa-house text-[#00bf00] dark:text-white transition-all duration-100 group-hover:text-white"></i>
-              {isTextVisible && (
-                <span
-                  className={`ml-3 text-[#4A494A] group-hover:text-white dark:text-white transition-all duration-200 ease-in-out transform overflow-hidden ${
-                    isTextVisible
-                      ? "opacity-100 translate-x-0 w-auto"
-                      : "opacity-0 translate-x-[-10px] w-0"
-                  }`}
-                >
-                  {t("home")}
-                </span>
-              )}
-            </a>
-          </li>
-
-          <li
-            className={`flex items-center px-3 py-2 text-gray-900 rounded hover:bg-[#00bf00] transition-all duration-100 hover:text-white hover:shadow-inner group border border-transparent dark:text-white dark:hover:border-white dark:hover:text-white group text-[14px] ${
-              isTextVisible ? "w-56 h-10" : "w-10 h-10 rounded-full"
-            }`}
+            <i className="fa-solid fa-plus"></i>
+            {isTextVisible && (
+              <span
+                className={`ml-2 transition-opacity duration-500 ease-in-out ${
+                  showText ? "opacity-100" : "opacity-0"
+                }`}
+              >
+                {t("create")}
+              </span>
+            )}
+          </button>
+        </div>
+      </aside>
+      <nav className="fixed bottom-0 left-0 w-full bg-white border-t border-[#A19FA1] dark:bg-[#4A494A] dark:border-gray-700 flex justify-around py-2 sm:hidden">
+        {[
+          { icon: "fa-house", label: t("home") },
+          { icon: "fa-users", label: t("community") },
+          { icon: "fa-bell", label: t("notifications") },
+          { icon: "fa-right-from-bracket", label: t("logout") },
+        ].map((item, index) => (
+          <a
+            key={index}
+            href="#"
+            className="flex flex-col items-center text-gray-900 dark:text-white transition-all duration-200 hover:text-[#00bf00]"
           >
-            <a
-              className={`flex items-center w-full h-full transition-opacity duration-500 ease-in-out ${
-                showText ? "opacity-100" : "opacity-0"
-              }`}
-              href="#"
-              title={!isTextVisible ? "Comunidades" : ""}
-            >
-              <i className="fa-solid fa-users text-[#00bf00] dark:text-white transition-all duration-100 group-hover:text-white"></i>
-              {isTextVisible && (
-                <span
-                  className={`ml-2 text-[#4A494A] group-hover:text-white dark:text-white transition-all duration-200 ease-in-out transform ${
-                    isTextVisible
-                      ? "opacity-100 translate-x-0"
-                      : "opacity-0 -translate-x-2 w-0 overflow-hidden"
-                  }`}
-                >
-                  {t("community")}
-                </span>
-              )}
-            </a>
-          </li>
-
-          <li
-            className={`flex items-center px-3 py-2 text-gray-900 rounded hover:bg-[#00bf00] transition-all duration-200 hover:text-white hover:shadow-inner group border border-transparent dark:text-white dark:hover:border-white dark:hover:text-white group text-[14px] ${
-              isTextVisible ? "w-56 h-10" : "w-10 h-10 rounded-full"
-            }`}
-          >
-            <a
-              className={`flex items-center w-full h-full transition-opacity duration-500 ease-in-out ${
-                showText ? "opacity-100" : "opacity-0"
-              }`}
-              href="#"
-              title={!isTextVisible ? "Notificaciones" : ""}
-            >
-              <i className="fa-solid fa-bell text-[#00bf00] dark:text-white transition-all duration-100 group-hover:text-white"></i>
-              {isTextVisible && (
-                <span
-                  className={`ml-3 text-[#4A494A] group-hover:text-white dark:text-white transition-all duration-200 ease-in-out transform ${
-                    isTextVisible
-                      ? "opacity-100 translate-x-0"
-                      : "opacity-0 -translate-x-2 w-0 overflow-hidden"
-                  }`}
-                >
-                  {t("notifications")}
-                </span>
-              )}
-            </a>
-          </li>
-
-          <li
-            className={`flex items-center px-3 py-2 text-gray-900 rounded hover:bg-[#00bf00] transition-all duration-100 hover:text-white hover:shadow-inner group border border-transparent dark:text-white dark:hover:border-white dark:hover:text-white group text-[14px] ${
-              isTextVisible ? "w-56 h-10" : "w-10 h-10 rounded-full"
-            }`}
-          >
-            <a
-              className={`flex items-center w-full h-full transition-opacity duration-500 ease-in-out ${
-                showText ? "opacity-100" : "opacity-0"
-              }`}
-              href="#"
-              title={!isTextVisible ? "Cerrar Sesión" : ""}
-            >
-              <i className="fa-solid fa-right-from-bracket ml-0.5 text-[#00bf00] dark:text-white transition-all duration-100 group-hover:text-white"></i>
-              {isTextVisible && (
-                <span
-                  className={`ml-3 text-[#4A494A] group-hover:text-white dark:text-white transition-all duration-200 ease-in-out transform ${
-                    isTextVisible
-                      ? "opacity-100 translate-x-0"
-                      : "opacity-0 -translate-x-2 w-0 overflow-hidden"
-                  }`}
-                >
-                  {t("logout")}
-                </span>
-              )}
-            </a>
-          </li>
-        </ul>
+            <i className={`fa-solid ${item.icon} text-lg`}></i>
+            <span className="text-[10px]">{item.label}</span>
+          </a>
+        ))}
         <button
-          title={!isTextVisible ? t("create") : ""}
+          title={t("create")}
           type="button"
-          className={`border hover:border-transparent bg-[#00bf00] text-white dark:hover:border-white font-medium rounded transition-all duration-200 ${
-            isTextVisible
-              ? "w-56 h-auto px-3 py-2 mt-3 text-[14px]"
-              : "w-10 h-10 mt-3 flex items-center justify-center text-[10px] rounded-full"
-          }`}
+          className="bg-[#00bf00] text-white w-11 h-11 rounded-full flex items-center justify-center shadow-lg transition-all duration-200 hover:scale-110"
         >
-          <i className="fa-solid fa-plus"></i>
-          {isTextVisible && (
-            <span
-              className={`ml-2 transition-opacity duration-500 ease-in-out ${
-                showText ? "opacity-100" : "opacity-0"
-              }`}
-            >
-              {t("create")}
-            </span>
-          )}
+          <i className="fa-solid fa-plus text-lg"></i>
         </button>
-      </div>
-    </aside>
+      </nav>
+    </>
   );
 }
