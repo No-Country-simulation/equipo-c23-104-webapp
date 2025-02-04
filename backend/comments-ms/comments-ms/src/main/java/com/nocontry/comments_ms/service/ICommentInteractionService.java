8d541a025dc9b89fcaf3cb983860dacd3227f841
Mereles Lautaro <lautaro.jobs@hotmail.com>
2025-02-04 16:32:27 -0300

    change configuration for dockerfile in eureka-sv

--- backend/comments-ms/comments-ms/src/main/java/com/nocontry/comments_ms/service/ICommentInteractionService.java
+++
@@ -1,8 +0,0 @@
-package com.nocontry.comments_ms.service;
-
-import com.nocontry.comments_ms.dto.CommentInteractionDto;
-
-public interface ICommentInteractionService {
-    boolean addInteraction(CommentInteractionDto interactionDTO);
-    boolean removeInteraction(CommentInteractionDto interactionDTO);
-}
