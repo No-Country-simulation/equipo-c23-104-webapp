053282cf290c62fc095a0bfb1d5fe2f38b7eb5cd
Anderson Forero <leidyusernameuwu@gmail.com>
2025-02-05 17:50:50 -0430

    Agregue Layout y modifique el Routes de App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import EditarPerfil from "./pages/EditarPerfil";
 import Seguidores from "./pages/Seguidores";
 import Welcome from './pages/welcome/Welcome';
 import Home from './pages/Home/Home';
-import Navbar from './components/Navbar';
+import Layout from './pages/Layout';
 import CommunitySelector from './pages/SelectorComunidad/SelectorComunidad';
 
 const App = () => {
@@ -14,15 +14,17 @@ const App = () => {
     <>
       <Routes>
         <Route path="/" element={ <Welcome/> } />
-        <Route path="home" element={<Home/>} />
+        <Route path="*" element={<Layout />}>
+          <Route path="home" element={<Home/>} />
+          <Route path="perfil" element={<Perfil/>}>
+            <Route path="editar" element={<EditarPerfil/>} />
+            <Route path="siguiendo" element={<Seguidores url="siguiendo" titulo="Siguiendo" boton={true} />} />
+            <Route path="seguidores" element={<Seguidores url="seguidores" titulo="Seguidores" boton={false} />} />
+          </Route>
+        </Route>
         <Route path="login" element={<Login/>} />
         <Route path="registrar" element={<Registrar/>} />
         <Route path="comunidades" element={<CommunitySelector/>} />
-        <Route path="perfil" element={<Perfil/>}>
-          <Route path="editar" element={<EditarPerfil/>} />
-          <Route path="siguiendo" element={<Seguidores url="siguiendo" titulo="Siguiendo" boton={true} />} />
-          <Route path="seguidores" element={<Seguidores url="seguidores" titulo="Seguidores" boton={false} />} />
-        </Route>
       </Routes>
     </>
   )
